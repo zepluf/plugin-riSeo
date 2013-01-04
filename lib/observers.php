55cde1bde5fc3b887034297037d7410a70810834
Tuan Nguyen <tuannguyen@rubikin.com>
2013-01-04 11:50:27 +0700

    Delete some files

--- lib/observers.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Thinh Nguyen
- * Date: 12/20/12
- * Time: 1:48 PM
- * To change this template use File | Settings | File Templates.
- */
-plugins\riPlugin\Plugin::get('riSeo.SeoObserver');
